5d9d29df49e7ce713ee6924d1ab79e2620f42602
Shanen Cross <shanencross@yahoo.com>
2021-02-09 20:27:39 -0800

    Pass tests 2 - 7
    Co-authored-by: Shannon Lee <shannonleehj@gmail.com>
    Co-authored-by: Marni Sucher <suchermarni@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
 function romanNumeral(number) {
-  return "I";
+	if (number === 1) {
+		return "I";
+	} else if (number === 5) {
+    return "V";
+  } else if (number === 10) {
+		return "X";
+	} else if (number === 50) {
+		return "L";
+	} else if (number === 100) {
+		return "C";
+	} else if (number === 500) {
+		return "D";
+	} else if (number === 1000) {
+		return "M";
+	}
 }
 
 
-console.log(romanNumeral(1));
+console.log(romanNumeral(5));
